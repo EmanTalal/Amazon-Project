166279cc565503976e8b7e6dc3dd18efd83899a0
github_EmanTalal <emantalal.201999@gmail.com>
2024-07-10 18:51:50 +0300

    MainRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import ProductPage from './pages/ProductPage';
+import Cart from './pages/Cart';
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import Checkout from './component/CheckOut';
+import Login from './pages/Login';
+import Sinup from './pages/Sinup';
+
+const router = createBrowserRouter([
+  {
+    path: '/Home',
+    element: <App />,
+  },
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/ProductPage/:id',
+    element: <ProductPage />,
+  },
+  {
+    path: '/Cart',
+    element: <Cart />,
+  },
+  {
+    path: '/checkout',
+    element: <Checkout />,
+  },
+  {
+    path: '/Sinup',
+    element: <Sinup />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
