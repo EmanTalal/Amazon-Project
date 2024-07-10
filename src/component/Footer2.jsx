00eb5707d2abdc539c808b70e485eec107402730
github_EmanTalal <emantalal.201999@gmail.com>
2024-07-10 18:50:19 +0300

    Footer

---
+++ src/component/Footer2.jsx
@@ -0,0 +1,17 @@
+function Footer2() {
+    return (
+      <>
+        <div className="footer footer-center bg-base-300 text-base-content p-4">
+          <aside>
+            <p className="text-white">
+              Copyright © ${new Date().getFullYear()} - All right reserved by
+              Amazon.com Industries Ltd
+            </p>
+          </aside>
+        </div>
+      </>
+    );
+  }
+  
+  export default Footer2;
+  
